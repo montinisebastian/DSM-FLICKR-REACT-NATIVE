5f898c6c68286392ce8dc601529dac129f3158ff
Montini Sebastian <montinisebastian@gmail.com>
2021-06-28 17:58:17 -0300

    Actualizando a Funcional PhotoList

--- components/PhotoList.js
+++ components/PhotoList.js
@@ -1,45 +1,43 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import {ScrollView, Text, View} from 'react-native';
 import axios from 'axios';
 import PhotoDetail from './PhotoDetail';
 
-class PhotoList extends Component {
-  state = {photos: null};
 
-  componentWillMount() {
-    axios
-      .get(
-        `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=6e8a597cb502b7b95dbd46a46e25db8d&photoset_id=${this.props.route.params.albumId}&user_id=137290658%40N08&format=json&nojsoncallback=1`,
-      )
-      .then((response) =>
-        this.setState({photos: response.data.photoset.photo}),
-      );
-  }
+const PhotoList = (props) => 
+{
 
-  renderAlbums() {
-    return this.state.photos.map((photo) => (
+  const [state, setState] = useState({photos: null});
+  const addPhotos = (e) => setState(e);
+  const renderAlbums = () => 
+  {
+    return state.photos.map((photo) => 
+    (
       <PhotoDetail
         key={photo.title}
         title={photo.title}
         imageUrl={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
       />
-    ));
+    )
+    );
   }
+  console.log(state);
 
-  render() {
-    console.log(this.state);
-
-    if (!this.state.photos) {
-      return (
-        <View style={{flex: 1}}>
-          <Text>Loading...</Text>
-        </View>
-      );
-    }
-
+  useEffect( () => 
+  {
+    axios
+    .get(
+      `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=6e8a597cb502b7b95dbd46a46e25db8d&photoset_id=${props.route.params.albumId}&user_id=137290658%40N08&format=json&nojsoncallback=1`,
+    )
+    .then((response) =>
+      addPhotos({photos: response.data.photoset.photo}),
+    );
+  }, []);
+  if (!state.photos) 
+  {
     return (
       <View style={{flex: 1}}>
-        <ScrollView>{this.renderAlbums()}</ScrollView>
+        <Text>LOADING...</Text>
       </View>
     );
   }
